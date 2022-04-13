846e0771eb496b85146085494f2a0dc772e8d17a
Kolawole <kushimo.kolawole@gmail.com>
2022-04-13 01:06:10 +0000

    updated job_activites

---
+++ target/run/dbt_practice/models/job_activities.sql
@@ -0,0 +1,18 @@
+
+  create view "dbt-practice"."dbt-practice-data"."job_activities__dbt_tmp" as (
+    WITH
+  jobactivities AS ( SELECT * FROM "dbt-practice"."dbt-practice-data_seeds"."job activities" ),
+
+  job_activities AS (
+
+    SELECT
+      id AS unique_id,
+      student_id,
+      category,
+      created_at
+    
+    FROM jobactivities
+  )
+
+SELECT * FROM job_activities
+  );
